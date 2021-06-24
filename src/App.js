5575ce36179fa9fbc221c5a3635bef385e45490a
tcee42 <toochukwuali@gmail.com>
2021-06-24 12:39:38 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,137 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Typography from "@material-ui/core/Typography";
+import { ThemeProvider } from "@material-ui/styles";
+import { createMuiTheme, makeStyles } from "@material-ui/core/styles";
+import { deepPurple } from "@material-ui/core/colors";
+import {
+  Grid,
+  Button,
+  Box,
+  Container,
+  Paper,
+  InputBase,
+} from "@material-ui/core";
+import AddIcon from "@material-ui/icons/Add";
+import FilterListIcon from "@material-ui/icons/FilterList";
+import SearchIcon from "@material-ui/icons/Search";
+import List from "./components/List";
+import "./App.css";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: deepPurple[900],
+    },
+    secondary: {
+      main: "#ffffff",
+    },
+  },
+});
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: "2px 4px",
+    display: "flex",
+    alignItems: "center",
+    width: 300,
+  },
+  input: {
+    marginLeft: theme.spacing(1),
+    flex: 1,
+  },
+  searchicon: {
+    margin: 4,
+  },
+}));
 
 function App() {
+  const [appData, setAppData] = useState({
+    loading: false,
+    rowsData: [],
+  });
+
+  useEffect(() => {
+    setAppData({ loading: true });
+    const url =
+      "http://javareesbyapi-env.eba-rtdeyeqd.ap-southeast-2.elasticbeanstalk.com/api/v1/getallclients/tenant/reesby";
+    axios.get(url).then((res) => {
+      const rows = res.data;
+      const rowsWithId = [...rows].map((row, i) => ({ ...row, id: i + 1 }));
+      setAppData({ loading: false, rowsData: rowsWithId });
+    });
+  }, []);
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Container maxWidth="lg">
+          <Typography variant="body2">MANAGEMENT</Typography>
+          <Grid
+            container
+            direction="row"
+            justify="space-between"
+            alignItems="center"
+          >
+            <Typography variant="h4">Clients</Typography>
+            <Button
+              variant="contained"
+              color="primary"
+              size="large"
+              component="span"
+              startIcon={<AddIcon />}
+            >
+              New Client
+            </Button>
+          </Grid>
+          <Grid
+            container
+            direction="row"
+            justify="space-between"
+            style={{ padding: "1rem 0" }}
+          >
+            <div className="btns-grid">
+              <Paper component="form" className={classes.root}>
+                <SearchIcon className={classes.searchicon} />
+
+                <InputBase
+                  className={classes.input}
+                  placeholder="Search"
+                  inputProps={{ "aria-label": "search" }}
+                />
+              </Paper>
+
+              <Button
+                style={{ height: "2.8rem" }}
+                variant="contained"
+                color="secondary"
+              >
+                Search
+              </Button>
+              <Button
+                variant="contained"
+                style={{ height: "2.8rem" }}
+                color="secondary"
+              >
+                Clear
+              </Button>
+            </div>
+            <Box>
+              <Button
+                variant="outlined"
+                color="primary"
+                size="large"
+                component="span"
+                startIcon={<FilterListIcon />}
+              >
+                Show Filter
+              </Button>
+            </Box>
+          </Grid>
+          <List rows={appData.rowsData} />
+        </Container>
+      </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/List.jsx
@@ -0,0 +1,29 @@
+import { DataGrid } from "@material-ui/data-grid";
+
+const columns = [
+  { field: "clientName", headerName: "Client Name", width: 170 },
+  { field: "clientEmail", headerName: "Email", width: 150 },
+  { field: "clientWorkPhone", headerName: "Phone", width: 130 },
+  { field: "clientIndustry", headerName: "Industry", width: 130 },
+  { field: "clientPocName", headerName: "Point of Contact", width: 200 },
+  { field: "clientWebsite", headerName: "Website", width: 150 },
+  { field: "edit", headerName: "", width: 130 },
+];
+
+const List = ({ rows }) => {
+  console.log(rows);
+  return (
+    <div style={{ height: 500, width: "100%" }}>
+      {rows ? (
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          pageSize={9}
+          checkboxSelection
+        />
+      ) : null}
+    </div>
+  );
+};
+
+export default List;
