95430161835be40c0a9cfa19506c4e07712d732d
tcee42 <toochukwuali@gmail.com>
2021-07-04 03:34:38 +0100

    restructures the project

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,31 @@
+import { useState, useEfect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 375,
+  },
+});
+
 const Login = () => {
-  return <div></div>;
+  const classes = useStyles();
+  return (
+    <Card className={classes.root}>
+      <CardContent>
+        <Typography variant="h5" component="h2"></Typography>
+
+        <Typography variant="body2" component="p"></Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+  );
 };
 
 export default Login;
